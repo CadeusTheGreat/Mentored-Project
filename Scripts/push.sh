4b9c2da1a79fcdf9074adff837bd35fb9c4e7b95
Cade Oldland <66471724+CadeusTheGreat@users.noreply.github.com>
2020-12-08 13:11:57 +0000

    Update push.sh

--- Scripts/push.sh
+++ Scripts/push.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
-sudo docker tag project_service1 localhost:8082/Service1
+sudo docker tag Service1 localhost:8082/Service1
 sudo docker push localhost:8082/Service1
 
-sudo docker tag project_service2 localhost:8082/Service2
+sudo docker tag Service2 localhost:8082/Service2
 sudo docker push localhost:8082/Service2
 
-sudo docker tag project_service3 localhost:8082/Service3
+sudo docker tag Service3 localhost:8082/Service3
 sudo docker push localhost:8082/Service3
 
-sudo docker tag project_service4 localhost:8082/Service4
+sudo docker tag Service4 localhost:8082/Service4
 sudo docker push localhost:8082/Service4
